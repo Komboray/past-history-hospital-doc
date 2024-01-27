ca7a3400f2ee4e5c877aeb7942caa41d21d9ecc0
Mbithi Raymond Kombo <121002402+Komboray@users.noreply.github.com>
2024-01-27 23:54:20 +0300

    Add files via upload

---
+++ past-history/pastHistory.php
@@ -0,0 +1,180 @@
+<?php
+
+$_POST["marital-status"];
+$_POST["reason-for-visit"];
+$_POST["ref-physician"];
+$_POST["occupation"];
+$_POST["phone-no"];
+//partners details
+$_POST["partner-name"];
+$_POST["partner-none"];
+$_POST["age-partner"];
+$_POST["part-occupation"];
+///////////////////////////////////PART B//////////////////////////
+$_POST["age-first-period"];
+$_POST["period-start-days"];
+$_POST["irreg-period-strt-dy"];
+$_POST["duration-bleeding"];
+$_POST["bleeding-btwn-periods"];
+$_POST["bleeding-after-periods"];
+$_POST["last-menstrual-period"];
+$_POST["pain-period"];
+$_POST["menses"];
+///////////////////////////////////////////////PART C
+$_POST["never-been-preg"];
+//table data below
+//table cell below
+$_POST["1st-ob-hist"];
+$_POST["place-of-del"];
+$_POST["duration-preg"];
+$_POST["hrs-labour"];
+$_POST["type-delivery"];
+$_POST["complications"];
+$_POST["sex"];
+$_POST["birth-weight"];
+$_POST["present-health"];
+//table cell
+//table cell below
+$_POST["2nd-ob-hist"];
+$_POST["place-of-del-2"];
+$_POST["duration-preg-2"];
+$_POST["hrs-labour-2"];
+$_POST["type-delivery-2"];
+$_POST["complications-2"];
+$_POST["sex-2"];
+$_POST["birth-weight-2"];
+$_POST["present-health-2"];
+//table cell below
+$_POST["3rd-ob-hist"];
+$_POST["place-of-del-3"];
+$_POST["duration-preg-3"];
+$_POST["hrs-labour-3"];
+$_POST["type-delivery-3"];
+$_POST["complications-3"];
+$_POST["sex-3"];
+$_POST["birth-weight-3"];
+$_POST["present-health-3"];
+//table cell below
+$_POST["4-ob-hist"];
+$_POST["place-of-del-4"];
+$_POST["duration-preg-4"];
+$_POST["hrs-labour-4"];
+$_POST["type-delivery-4"];
+$_POST["complications-4"];
+$_POST["sex-4"];
+$_POST["birth-weight-4"];
+$_POST["present-health-4"];
+//table cell below
+$_POST["5-ob-hist"];
+$_POST["place-of-del-5"];
+$_POST["duration-preg-5"];
+$_POST["hrs-labour-5"];
+$_POST["type-delivery-5"];
+$_POST["complications-5"];
+$_POST["sex-5"];
+$_POST["birth-weight-5"];
+$_POST["present-health-5"];
+/////////////////////////////////////////////////////D PART D
+$_POST["birth-control"];
+/////////////////////////////////////////////////////E PART E
+$_POST["sex-partner"];//this is a no or yes
+$_POST["sex-partner-male"];//this is after you answer yes if male or female
+$_POST["sex-concerns"];
+//F
+$_POST["D&C"];
+$_POST["D&C-date"];
+$_POST["Hysteroscopy"];
+$_POST["Hysteroscopy-date"];
+$_POST["infertility-surgery"];
+$_POST["infertility-surgery-date"];
+$_POST["tuboplasty"];
+$_POST["tuboplasty-date"];
+$_POST["tubal-ligation"];
+$_POST["tubal-ligation-date"];
+$_POST["hysterectomy-(vaginal)"];
+$_POST["hysterectomy-(vaginal)-date"];
+$_POST["hysterectomy-(abdominal)"];
+$_POST["hysterectomy-(abdominal)-date"];
+$_POST["myomectomy"];
+$_POST["myomectomy-date"];
+$_POST["ovarian"];
+$_POST["ovarian-date"];
+$_POST["L-cyst(s)"];
+$_POST["L-cyst(s)-date"];
+$_POST["R-ovary"];
+$_POST["R-ovary-date"];
+$_POST["L-ovary"];
+$_POST["L-ovary-date"];
+$_POST["vaginal-or-bladder-repair"];
+$_POST["vaginal-or-bladder-repair-date"];
+$_POST["cesarean"];
+$_POST["cesarean-date"];
+$_POST["other-(specify)"];
+$_POST["other-(specify)-date"];
+//////////////////////////////////////////////////////GGGG
+$_POST["surgeries-no"];
+$_POST["surg-1"];
+$_POST["surg-1-date"];
+$_POST["surg-2"];
+$_POST["surg-2-date"];
+$_POST["surg-3"];
+$_POST["surg-3-date"];
+$_POST["surg-4"];
+$_POST["surg-4-date"];
+////////////////////////////////////////////////////////HHHH
+$_POST["date-of-pap-check"];
+$_POST["date-of-pap"];
+$_POST["abnormal-pap-smears-check"];
+$_POST["abnormal-pap-smears"];//yes or no
+//if yes then the below options
+$_POST["cryotherapy"];
+$_POST["laser"];
+$_POST["cone-biopsy"];
+$_POST["loop-excision"];
+$_POST["last-mammogram"];//this is a date
+$_POST["ab-mamo-no"];//this is a yes or no
+//other past gyno history//check any that apply
+$_POST["gyn-hist-None"];
+$_POST["gyn-hist-Venereal"];
+$_POST["gyn-hist-Herpes"];
+$_POST["gyn-hist-Syphilis"];
+$_POST["gyn-hist-Pelvic"];
+$_POST["gyn-hist-Endometriosis"];
+$_POST["gyn-hist-Chlamydia"];
+$_POST["gyn-hist-Gonorrhea"];
+$_POST["gyn-hist-Vaginal"];
+$_POST["gyn-other"];
+
+///////////////////////////////////////////////////I
+///PAST MED HIST
+$_POST["past-med-hist"];//none
+$_POST[""];
+$_POST[""];
+$_POST[""];
+$_POST[""];
+$_POST[""];
+$_POST[""];
+$_POST[""];
+$_POST[""];
+$_POST[""];
+$_POST[""];
+$_POST[""];
+$_POST[""];
+$_POST[""];
+$_POST[""];
+$_POST[""];
+$_POST[""];
+$_POST[""];
+$_POST[""];
+$_POST[""];
+$_POST[""];
+$_POST[""];
+$_POST[""];
+$_POST[""];
+$_POST[""];
+$_POST[""];
+$_POST[""];
+$_POST[""];
+$_POST[""];
+$_POST[""];
+$_POST[""];
